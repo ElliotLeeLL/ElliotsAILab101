ae370952abc670244555dce55193e75279e10850
ElliotLeeLL <xingyuli1996@gmail.com>
2025-03-22 00:25:33 +0800

    Test tensorflow dependency

--- demo.py
+++ demo.py
@@ -1,3 +1,33 @@
+#%%
 import tensorflow as tf
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
 
-print(tf.config.list_physical_devices('GPU'))
+# Preparing the data
+housing = fetch_california_housing()
+X_train_full, X_test, y_train_full, y_test = train_test_split(
+    housing.data, housing.target, random_state=42)
+X_train, X_valid, y_train, y_valid = train_test_split(
+    X_train_full, y_train_full, random_state=42)
+
+# Preprocessing the data
+tf.keras.utils.set_random_seed(42)
+norm_layer = tf.keras.layers.Normalization()
+
+# Building the model
+hidden_layer1 = tf.keras.layers.Dense(30, activation="relu")
+hidden_layer2 = tf.keras.layers.Dense(30, activation="relu")
+concat_layer = tf.keras.layers.Concatenate()
+output_layer = tf.keras.layers.Dense(1)
+
+input_ = tf.keras.layers.Input(shape=X_train.shape[1:])
+normalized = norm_layer(input_)
+hidden1 = hidden_layer1(normalized)
+hidden2 = hidden_layer2(hidden1)
+concat = concat_layer([input_, hidden2])
+output = output_layer(concat)
+
+model = tf.keras.Model(inputs=[input_], outputs=[output])
+
+model.summary()
+
